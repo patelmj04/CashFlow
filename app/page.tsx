dda8fb6ae80fca343824da7782fdf17c31ac0c9b
Mit Patel <132839819+patelmj04@users.noreply.github.com>
2025-03-18 00:10:05 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -637,7 +637,7 @@ export default function Home() {
           <div className="mt-8 border-t border-gray-700 pt-8 flex flex-col md:flex-row justify-between items-center">
             <p className="text-xs text-gray-400">© 2025 CashFlow. All rights reserved.</p>
             <div className="flex space-x-4 mt-4 md:mt-0">
-              <Link href="#" className="text-gray-400 hover:text-white">
+              <Link href="patelmj04" className="text-gray-400 hover:text-white">
                 <span className="sr-only">Facebook</span>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -654,7 +654,7 @@ export default function Home() {
                   <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
                 </svg>
               </Link>
-              <Link href="#" className="text-gray-400 hover:text-white">
+              <Link href="https://x.com/patelmj04" className="text-gray-400 hover:text-white">
                 <span className="sr-only">Twitter</span>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -671,7 +671,7 @@ export default function Home() {
                   <path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z"></path>
                 </svg>
               </Link>
-              <Link href="#" className="text-gray-400 hover:text-white">
+              <Link href="https://www.instagram.com/its_mit_btw/" className="text-gray-400 hover:text-white">
                 <span className="sr-only">Instagram</span>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -690,7 +690,7 @@ export default function Home() {
                   <line x1="17.5" x2="17.51" y1="6.5" y2="6.5"></line>
                 </svg>
               </Link>
-              <Link href="#" className="text-gray-400 hover:text-white">
+              <Link href="https://www.linkedin.com/in/patelmj04/" className="text-gray-400 hover:text-white">
                 <span className="sr-only">LinkedIn</span>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
